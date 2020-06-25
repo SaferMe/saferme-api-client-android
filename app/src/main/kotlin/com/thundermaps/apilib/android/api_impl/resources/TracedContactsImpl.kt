a8a8d370e3723c09d9234bc10024df596bd3d8b2
pedro teran <pedro.gezn@gmail.com>
2020-06-25 12:40:09 +1200

    added contact tracing support

---
+++ app/src/main/kotlin/com/thundermaps/apilib/android/api_impl/resources/TracedContactsImpl.kt
@@ -0,0 +1,21 @@
+package com.thundermaps.apilib.android.api_impl.resources
+
+import com.thundermaps.apilib.android.api.requests.RequestParameters
+import com.thundermaps.apilib.android.api.requests.SaferMeApiResult
+import com.thundermaps.apilib.android.api.resources.TracedContacts
+import com.thundermaps.apilib.android.api.resources.TracedContactsResource
+import com.thundermaps.apilib.android.api_impl.AndroidClient
+
+class TracedContactsImpl(val api: AndroidClient) : TracedContactsResource {
+
+    override suspend fun create(
+        parameters: RequestParameters,
+        item: TracedContacts,
+        success: (SaferMeApiResult<TracedContacts>) -> Unit,
+        failure: (Exception) -> Unit
+    ) {
+        StandardMethods.create(
+            api = api, path=  "traced_contacts", parameters  = parameters, item = item, success= success, failure =  failure
+        )
+    }
+}
---
+++ app/src/test/kotlin/com/thundermaps/apilib/android/api_impl/resources/TracedContactsTest.kt
@@ -0,0 +1,74 @@
+package com.thundermaps.apilib.android.api_impl.resources
+
+import com.thundermaps.apilib.android.api.resources.TracedContact
+import com.thundermaps.apilib.android.api.resources.TracedContacts
+import com.thundermaps.apilib.android.api_impl.AndroidClient
+import io.ktor.client.request.HttpRequestBuilder
+import io.ktor.http.ContentType
+import io.ktor.http.HttpStatusCode
+import io.ktor.http.headersOf
+import io.ktor.util.KtorExperimentalAPI
+import io.mockk.MockKAnnotations
+import io.mockk.every
+import io.mockk.impl.annotations.MockK
+import junit.framework.TestCase
+import kotlinx.coroutines.runBlocking
+import org.junit.Before
+import org.junit.Test
+
+class TracedContactsTest {
+
+    @MockK
+    lateinit var defaultAPI: AndroidClient
+
+    @Before
+    fun setUp() {
+        MockKAnnotations.init(this)
+    }
+
+    /**
+     * Test that calling this method will cause a valid call to StandardMethods.create
+     * The tests in [StandardCreateTest] cover nearly all functionality - we only need to consider
+     * that it build an object of the correct type when successful
+     */
+    @KtorExperimentalAPI
+    @Test
+    fun testCreateSuccess() {
+        var tracedContacts = ArrayList<TracedContact>()
+        tracedContacts.add(TracedContact("1", "bluetooth", "123", "", 3))
+        val returnObject = "{\"message\":\"accepted\"}"
+        val responseHeaders =
+            headersOf("Content-Type" to listOf(ContentType.Application.Json.toString()))
+        var count = 0
+        var inspectCalled = false
+        val expectedPath = "/api/v0/traced_contacts"
+
+        val client = TestHelpers.testClient(
+            content = returnObject,
+            status = HttpStatusCode.Accepted,
+            headers = responseHeaders,
+            // ensure the correct path is used
+            requestInspector = {
+                TestCase.assertEquals(it.url.encodedPath, expectedPath)
+                inspectCalled = true
+            }
+        )
+        every {
+            defaultAPI.client(any())
+        } answers {
+            Pair(client, HttpRequestBuilder())
+        }
+
+        runBlocking {
+            TracedContactsImpl(defaultAPI).create(TestHelpers.defaultParams, TracedContacts(tracedContacts),
+                {
+                    TestCase.assertNotNull(it.data)
+                    synchronized(count) { count++ }
+                }, {
+                    TestCase.fail("Failure block should not be called")
+                })
+        }
+
+        TestCase.assertTrue(inspectCalled)
+    }
+}
