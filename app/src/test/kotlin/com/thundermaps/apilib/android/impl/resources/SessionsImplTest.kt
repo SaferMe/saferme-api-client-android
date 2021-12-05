9bd50c8609e1991b38b4facee1df0c8eb8391f15
Liem Vo <liem.vo@safer.me>
2021-12-06 11:44:25 +1300

    SM-4154 WIP add unit tests: Test SessionsImpl

---
+++ app/src/test/kotlin/com/thundermaps/apilib/android/impl/resources/SessionsImplTest.kt
@@ -0,0 +1,17 @@
+package com.thundermaps.apilib.android.impl.resources
+
+import org.junit.Assert.*
+
+import org.junit.After
+import org.junit.Before
+
+class SessionsImplTest {
+
+    @Before
+    fun setUp() {
+    }
+
+    @After
+    fun tearDown() {
+    }
+}
