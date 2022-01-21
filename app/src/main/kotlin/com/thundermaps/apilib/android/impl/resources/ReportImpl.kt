5a36ec8b763d8ba7170731097f6ee77e686244cc
Liem Vo <liem.vo@safer.me>
2022-01-21 22:21:55 +1300

    SM-4365 align filed with ios app

--- app/src/main/kotlin/com/thundermaps/apilib/android/impl/resources/ReportImpl.kt
+++ app/src/main/kotlin/com/thundermaps/apilib/android/impl/resources/ReportImpl.kt
@@ -66,6 +66,6 @@ class ReportImpl(val api: AndroidClient) : ReportResource {
 
     companion object {
         private const val REPORT_PATH = "reports"
-        private const val FIELDS_PARAM = "fields=categories_title,comment_count,viewer_count,form_fields,hidden_fields,is_hazard,risk_level,risk_assessment,risk_matrix_config,risk_control_id,risk_control_editable_by"
+        private const val FIELDS_PARAM = "fields=categories_title,comment_count,viewer_count,form_fields,hidden_fields,is_hazard,risk_level,risk_assessment,risk_matrix_config,risk_control_id,risk_control_editable_by,report_state"
     }
 }
