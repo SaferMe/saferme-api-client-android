673b8636d08ca681b74d5cae7aa41928132ebe9a
Liem Vo <liem.vo@safer.me>
2022-01-24 11:08:45 +1300

    SM-4365 Improve exclude jacoco annotation

--- app/src/main/kotlin/com/thundermaps/apilib/android/api/ExcludeFromJacocoGeneratedReport.kt
+++ app/src/main/kotlin/com/thundermaps/apilib/android/api/ExcludeFromJacocoGeneratedReport.kt
@@ -1,13 +1,19 @@
 package com.thundermaps.apilib.android.api
 
 import java.lang.annotation.Retention
-import java.lang.annotation.RetentionPolicy
 
-@Retention(RetentionPolicy.RUNTIME)
+@kotlin.annotation.Retention(AnnotationRetention.RUNTIME)
 @Target(
     AnnotationTarget.FUNCTION,
     AnnotationTarget.PROPERTY_GETTER,
     AnnotationTarget.PROPERTY_SETTER,
-    AnnotationTarget.CLASS
+    AnnotationTarget.CONSTRUCTOR,
+    AnnotationTarget.FIELD,
+    AnnotationTarget.PROPERTY,
+    AnnotationTarget.CLASS,
+    AnnotationTarget.FILE,
+    AnnotationTarget.TYPEALIAS,
+    AnnotationTarget.TYPE,
+    AnnotationTarget.TYPE_PARAMETER
 )
 annotation class ExcludeFromJacocoGeneratedReport
