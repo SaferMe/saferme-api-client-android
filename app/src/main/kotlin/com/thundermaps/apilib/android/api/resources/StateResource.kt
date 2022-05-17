90547acf4c4742917f72a9ac82524310da70c471
Liem Vo <liem.vo@safer.me>
2022-05-17 17:30:56 +1200

    SM-4539 update state service

--- app/src/main/kotlin/com/thundermaps/apilib/android/api/resources/StateResource.kt
+++ app/src/main/kotlin/com/thundermaps/apilib/android/api/resources/StateResource.kt
@@ -2,4 +2,4 @@ package com.thundermaps.apilib.android.api.resources
 
 import com.thundermaps.apilib.android.api.responses.models.ReportState
 
-interface StateResource: SaferMeResource<ReportState>, Readable<ReportState>, Indexable<ReportState>
+interface StateResource : SaferMeResource<ReportState>, Readable<ReportState>, Indexable<ReportState>
