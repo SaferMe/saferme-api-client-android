82b29093691b2973ec818a02bae1aeabc1321551
Meet Parabiya <99625082+meet-saferme@users.noreply.github.com>
2022-06-07 10:49:41 +0530

    removed change profile picture changes
    removed change profile picture changes

--- app/src/main/kotlin/com/thundermaps/apilib/android/impl/resources/MeResourceImpl.kt
+++ app/src/main/kotlin/com/thundermaps/apilib/android/impl/resources/MeResourceImpl.kt
@@ -18,20 +18,12 @@ import com.thundermaps.apilib.android.impl.AndroidClient
 import io.ktor.client.call.HttpClientCall
 import io.ktor.client.call.call
 import io.ktor.client.request.HttpRequestBuilder
-import io.ktor.client.request.forms.formData
-import io.ktor.client.request.forms.submitFormWithBinaryData
 import io.ktor.client.request.url
-import io.ktor.client.response.HttpResponse
 import io.ktor.http.contentType
 import io.ktor.http.ContentType
-import io.ktor.http.Headers
-import io.ktor.http.HttpHeaders
 import io.ktor.http.HttpMethod
 import io.ktor.util.KtorExperimentalAPI
-import java.io.File
-import java.math.BigInteger
 import java.net.UnknownHostException
-import java.util.Random
 import javax.inject.Inject
 import javax.inject.Singleton
 
@@ -157,29 +149,6 @@ class MeResourceImpl @Inject constructor(
             }
         })
         return call
-    }
-
-    private suspend fun <T : Any> processCallForAvatar(
-        parameters: RequestParameters,
-        path: String = USER_PATH,
-        query: String = "",
-        file:File
-    ): HttpResponse {
-        val (client, requestBuilder) = androidClient.client(parameters)
-
-        val boundary: String = BigInteger(35, Random()).toString()
-        val response: HttpResponse = client.submitFormWithBinaryData(
-            url = AndroidClient.baseUrlBuilder(parameters).apply {
-                encodedPath = "${encodedPath}$path$query"
-            }.build().encodedPath,
-            formData = formData {
-                append("avatar", file.readBytes(), Headers.build {
-                    append(HttpHeaders.ContentType, "image/png")
-                    append(HttpHeaders.ContentDisposition, "filename=\"${file.name}\"")
-                })
-            }
-        )
-        return response
     }
 
     companion object {
