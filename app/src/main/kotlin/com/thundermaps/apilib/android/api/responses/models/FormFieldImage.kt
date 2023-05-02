c92fe5c98c98b739a25ec2cd1da4597cd3fd6d0c
Jing ZHANG <jing.zhang@safer.me>
2023-05-02 14:25:20 +1200

    Add file_attachment get updated url api
    SM-6655

--- app/src/main/kotlin/com/thundermaps/apilib/android/api/responses/models/FormFieldImage.kt
+++ app/src/main/kotlin/com/thundermaps/apilib/android/api/responses/models/FormFieldImage.kt
@@ -1,17 +1,4 @@
 
 package com.thundermaps.apilib.android.api.responses.models
 
-import com.google.gson.annotations.Expose
-import com.google.gson.annotations.SerializedName
-import com.thundermaps.apilib.android.api.ExcludeFromJacocoGeneratedReport
-
-@ExcludeFromJacocoGeneratedReport
-data class FormFieldImage(
-    @Expose val id: Int,
-    @SerializedName("original_url") @Expose val originalUrl: String,
-    @Expose val filename: String?,
-    @SerializedName("style_url") @Expose val styleUrl: ImageStyleUrl?
-)
-
-@ExcludeFromJacocoGeneratedReport
-data class ImageStyleUrl(@Expose val medium: String, @Expose val thumb: String)
+typealias FormFieldImage = FileAttachment
