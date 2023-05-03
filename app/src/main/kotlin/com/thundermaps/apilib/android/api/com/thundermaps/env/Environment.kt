ab7480fa3c573184d29305dbcf1f13450c1de942
Jing ZHANG <jing.zhang@safer.me>
2023-05-03 17:11:19 +1200

    Add seperate api servers for oceanfarmr brand
    SM-6659

--- app/src/main/kotlin/com/thundermaps/apilib/android/api/com/thundermaps/env/Environment.kt
+++ app/src/main/kotlin/com/thundermaps/apilib/android/api/com/thundermaps/env/Environment.kt
@@ -13,3 +13,16 @@ object Live : Environment(
 object Staging : Environment(
     listOf("api.staging.saferme.io", "api1.staging.saferme.io")
 )
+
+object LiveOceanfarmr : Environment(
+    listOf(
+        "api1.prod.infra.oceanfarmr.com",
+        "api2.prod.infra.oceanfarmr.com",
+        "api3.prod.infra.oceanfarmr.com",
+        "api4.prod.infra.oceanfarmr.com"
+    )
+)
+
+object StagingOceanfarmr : Environment(
+    listOf("api.staging.infra.oceanfarmr.com", "api1.staging.infra.oceanfarmr.com")
+)
