c2125652993ba5280040b3da6f45747ba6686ffd
Jing ZHANG <jing.zhang@safer.me>
2023-06-18 13:56:18 +1200

    Refactor requestParameter api
    SM-42

--- app/src/main/kotlin/com/thundermaps/apilib/android/impl/resources/BrandResourceImpl.kt
+++ app/src/main/kotlin/com/thundermaps/apilib/android/impl/resources/BrandResourceImpl.kt
@@ -1,38 +1,19 @@
 package com.thundermaps.apilib.android.impl.resources
 
-import apiRequest
-import com.thundermaps.apilib.android.api.com.thundermaps.env.EnvironmentManager
-import com.thundermaps.apilib.android.api.requests.Constants.APPLICATION_JSON
-import com.thundermaps.apilib.android.api.requests.RequestParameters
 import com.thundermaps.apilib.android.api.resources.BrandResource
 import com.thundermaps.apilib.android.api.responses.models.Brand
 import com.thundermaps.apilib.android.api.responses.models.Result
 import com.thundermaps.apilib.android.impl.AndroidClient
-import com.thundermaps.apilib.android.impl.HeaderType
-import io.ktor.http.HttpHeaders
+import com.thundermaps.apilib.android.impl.apiRequest
 import javax.inject.Inject
 import javax.inject.Singleton
 
 @Singleton
 class BrandResourceImpl @Inject constructor(
-    private val androidClient: AndroidClient,
-    private val environmentManager: EnvironmentManager
+    private val androidClient: AndroidClient
 ) : BrandResource {
-    fun createParameters(applicationId: String, host: String = "") =
-        RequestParameters(
-            customRequestHeaders = hashMapOf(
-                HeaderType.xAppId to applicationId,
-                HttpHeaders.Accept to APPLICATION_JSON
-            ),
-            credentials = null,
-            host = host,
-            api_version = 4,
-            parameters = mapOf("fields" to BRAND_FIELDS.joinToString(","))
-        )
-
     override suspend fun getBrand(applicationId: String): Result<Brand> {
-        val parameters = createParameters(applicationId, environmentManager.environment.servers.first())
-        val (client, requestBuilder) = androidClient.build(parameters, PATH)
+        val (client, requestBuilder) = androidClient.buildRequest(mapOf("fields" to BRAND_FIELDS.joinToString(",")), PATH)
         return client.apiRequest(requestBuilder)
     }
 
