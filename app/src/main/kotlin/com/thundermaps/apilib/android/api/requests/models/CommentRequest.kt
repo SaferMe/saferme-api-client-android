6143af2f6c7d5c15c45f32c8fd0337ab73d9e18f
Eunice Llobet <eunice.llobet@safer.me>
2023-08-10 16:19:18 +1200

    fix request

--- app/src/main/kotlin/com/thundermaps/apilib/android/api/requests/models/CommentRequest.kt
+++ app/src/main/kotlin/com/thundermaps/apilib/android/api/requests/models/CommentRequest.kt
@@ -1,10 +1,16 @@
 package com.thundermaps.apilib.android.api.requests.models
 
 import com.google.gson.annotations.Expose
+import com.google.gson.annotations.SerializedName
+
+data class ReportCommentRequest(
+    @SerializedName("uuid") @Expose val uuid: String? = null,
+    @SerializedName("content") @Expose val content: String? = null,
+    @SerializedName("is_anonymous") @Expose val isAnonymous: Boolean? = null,
+    @SerializedName("user_id") @Expose val userId: Int? = null,
+    @SerializedName("report_uuid") @Expose val reportUuid: String? = null
+)
 
 data class CommentRequest(
-    @Expose val uuid: String? = null,
-    @Expose val content: String? = null,
-    @Expose val isAnonymous: Boolean? = null,
-    @Expose val reportId: Int? = null
+    @SerializedName("report_comment") @Expose val reportComment: ReportCommentRequest? = null
 )
